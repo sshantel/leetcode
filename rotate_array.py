9d1f526ac784714e7b6bbe4ae3d69f29f09d77b7
Chantel Yip <chantelyip422@gmail.com>
2020-09-11 15:30:33 -0700

    completed function

--- rotate_array.py
+++ rotate_array.py
@@ -5,24 +5,21 @@ Follow up:
 
 Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
 Could you do it in-place with O(1) extra space?
+
+
+
+
 """
+from typing import List 
 
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-    def rotate(nums, k):
-        new_nums_right = []
-        new_nums_left = []
-        length_nums = len(nums)
-        d = (len(nums)) - k 
+def rotate(nums: List[int], k: int) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """  
+    k = k % len(nums)
+    answer = nums[-k:] + nums[:-k] 
+    for i in range(len(nums)):
+        nums[i] = answer[i]
+    return nums
 
-        new_nums_right.extend(nums[d:]) 
-        new_nums_left.extend(nums[:d]) 
-        merged_list = []
-        merged_list.extend(new_nums_right)
-        merged_list.extend(new_nums_left)
-        print(merged_list) 
-
-    rotate([1,2,3,4,5,6,7], 3)
+print(rotate([1,2,3,4,5,6,7], 3))
