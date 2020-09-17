9b0a68488775c3eab679ef6ddb7e7d6801fea51e
Chantel Yip <chantelyip422@gmail.com>
2020-09-17 11:01:17 -0700

    completed function

--- 231_power_of_two.py
+++ 231_power_of_two.py
@@ -2,8 +2,16 @@
 Given an integer, write a function to determine if it is a power of two.
 """
 
-import math
+# import math
+
+# def isPowerOfTwo(n):
+#     return n > 0 and math.log2(n) % 1 == 0
+# print(isPowerOfTwo(64))
 
 def isPowerOfTwo(n):
-    return n > 0 and math.log2(n) % 1 == 0
-print(isPowerOfTwo(64))
+    i = 0
+    while i < n:
+        if n == pow(2, i):
+            return True
+        i += 1
+    return False
