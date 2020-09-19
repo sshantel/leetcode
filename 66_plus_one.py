240849f606f18c48e08b9eebb14379a682c35b7c
Chantel Yip <chantelyip422@gmail.com>
2020-09-18 17:37:22 -0700

    improved runtime on function

--- 66_plus_one.py
+++ 66_plus_one.py
@@ -6,6 +6,22 @@ The digits are stored such that the most significant digit is at the head of the
 You may assume the integer does not contain any leading zero, except the number 0 itself.
 
 """ 
-def plusOne(digits):
-    last = [digits[-1]+1]
-    return digits[:-1] + last
+from typing import List
+def plusOne(digits: List[int]) -> List[int]:
+    if digits[-1] < 9:
+        digits[-1] += 1
+        return digits 
+    elif digits[-1] == 9:
+        i = -1
+        while digits[i] == 9:
+            digits[i] = 0
+            try:
+                if digits[i - 1] < 9:
+                    digits[i - 1] += 1
+                else:
+                    i -= 1
+            except: return [1] + digits
+        return digits
+    else: raise ValueError
+print(plusOne([9,9,9]))
+print(plusOne([8,9,9]))
