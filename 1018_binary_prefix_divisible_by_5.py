30609135995789008884b0dc6b84c0ed152f9801
Chantel Yip <chantelyip422@gmail.com>
2020-09-18 22:04:22 -0700

    fixed function

--- 1018_binary_prefix_divisible_by_5.py
+++ 1018_binary_prefix_divisible_by_5.py
@@ -4,22 +4,10 @@ Given an array A of 0s and 1s, consider N_i: the i-th subarray from A[0] to A[i]
 Return a list of booleans answer, where answer[i] is true if and only if N_i is divisible by 5.
 """
 
-from typing import List
-def plusOne(digits: List[int]) -> List[int]:
-    if digits[-1] < 9:
-        digits[-1] += 1
-        return digits 
-    elif digits[-1] == 9:
-        i = -1
-        while digits[i] == 9:
-            digits[i] = 0
-            try:
-                if digits[i - 1] < 9:
-                    digits[i - 1] += 1
-                else:
-                    i -= 1
-            except: return [1] + digits
-        return digits
-    else: raise ValueError
-print(plusOne([9,9,9]))
-print(plusOne([8,9,9]))
+def prefixesDivBy5(self, A):
+    remainder = 0
+    answers = len(A) * [None]
+    for i in range(len(A)):
+        remainder = ((remainder << 1) + A[i]) % 5
+        answers[i] = (remainder == 0)
+    return answers
