a925453fc20a3d4e62f5767fdf08cf446cf9e4e3
Chantel Yip <chantelyip422@gmail.com>
2020-10-16 11:02:37 -0700

    added doctests

--- 1408_string_matching_in_an_array.py
+++ 1408_string_matching_in_an_array.py
@@ -2,4 +2,14 @@
 Given an array of string words. Return all strings in words which is substring of another word in any order. 
 
 String words[i] is substring of words[j], if can be obtained removing some characters to left and/or right side of words[j].
+
+>>> stringMatching(["mass","as","hero","superhero"])
+["as","hero"]
+
+>>> stringMatching(["leetcode","et","code"])
+["et","code"]
+
+>>> stringMatching["blue","green","bu"]
+[]
+
 """
