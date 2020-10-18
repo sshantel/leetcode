c28a70ee8a84398d8eaccb219dd080519062384f
Chantel Yip <chantelyip422@gmail.com>
2020-10-17 21:16:48 -0700

    initial commit

--- 1408_string_matching_in_an_array.py
+++ 1408_string_matching_in_an_array.py
@@ -4,12 +4,26 @@ Given an array of string words. Return all strings in words which is substring o
 String words[i] is substring of words[j], if can be obtained removing some characters to left and/or right side of words[j].
 
 >>> stringMatching(["mass","as","hero","superhero"])
-["as","hero"]
+['as', 'hero']
 
 >>> stringMatching(["leetcode","et","code"])
-["et","code"]
+['et', 'code']
 
 >>> stringMatching["blue","green","bu"]
 []
 
 """
+
+def stringMatching(words):
+    l = []
+    for i in words:
+        for j in words:
+            if j in i and j!= i:
+                if j not in l:
+                    l.append(j)
+    return l
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n✨ ALL TESTS PASSED!\n')
