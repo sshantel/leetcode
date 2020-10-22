bd196f8343334cf41295fbec0472caa85085f0ac
Chantel Yip <chantelyip422@gmail.com>
2020-10-22 00:04:48 -0700

    added doctests

--- 1281_subtract_the_product_and_sum_of_digits_of_an_integer.py
+++ 1281_subtract_the_product_and_sum_of_digits_of_an_integer.py
@@ -1,3 +1,24 @@
 """
 Given an integer number n, return the difference between the product of its digits and the sum of its digits.
-"""
+
+>>> subtractProductAndSum(234)
+15
+
+>>> subtractProductAndSum(4421)
+21
+
+""" 
+def subtractProductAndSum(n):
+    s= str(n)
+    sum=0
+    product=1
+    for i in range(0,len(s)):
+        sum+=int(s[i])
+        product*= int(s[i])
+    return product-sum
+
+if __name__ == "__main__":
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED.\n")
+            
