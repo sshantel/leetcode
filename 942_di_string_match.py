4e9dcb30cd1ec78eb93a17d81b3559b16195786e
Chantel Yip <chantelyip422@gmail.com>
2020-11-07 17:08:02 -0800

    added doctests

---
+++ 942_di_string_match.py
@@ -0,0 +1,31 @@
+"""
+Given a string S that only contains "I" (increase) or "D" (decrease), let N = S.length.
+
+Return any permutation A of [0, 1, ..., N] such that for all i = 0, ..., N-1:
+
+If S[i] == "I", then A[i] < A[i+1]
+If S[i] == "D", then A[i] > A[i+1]
+ 
+"""
+
+ 
+def diStringMatch(S):
+    mx = len(S)
+    mi = 0
+    r = [] 
+     
+    for c in S:
+        if c == 'I':
+            r.append(mi)
+            mi += 1
+        else:
+            r.append(mx)
+            mx -= 1
+    r.append(mx)
+    return r
+
+if __name__ == "__main__":
+    import doctest
+
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED.\n")
