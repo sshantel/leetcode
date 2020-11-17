45c89e73c395e0f73f3b1891deadd4c202dc2d77
Chantel Yip <chantelyip422@gmail.com>
2020-11-17 15:49:11 -0800

    added doctests

---
+++ 283_move_zeroes.py
@@ -0,0 +1,24 @@
+"""
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+moveZeroes([0,1,0,3,12])
+[1,3,12,0,0]
+
+"""
+ 
+def moveZeroes(nums):
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    i = 0
+    for num in range(len(nums)-1):
+         if nums[i] == 0: 
+            nums.append(nums.pop(i))
+            i -= 1
+         i += 1
+
+
+if __name__ == "__main__":
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED.\n")
