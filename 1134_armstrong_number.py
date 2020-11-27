1d3e7f89866d931f059a08400f604256cc65a4a6
Chantel Yip <chantelyip422@gmail.com>
2020-11-27 10:24:49 -0800

    added doctests and added description of armstrong problem

--- 1134_armstrong_number.py
+++ 1134_armstrong_number.py
@@ -1,4 +1,15 @@
+"""
+The k-digit number N is an Armstrong number if and only if the k-th power of each digit sums to N.
 
+Given a positive integer N, return true if and only if it is an Armstrong number.
+
+<<< isArmstrong(153)
+True
+
+<<< isArmstrong(123)
+False
+
+"""
 def isArmstrong(N):
     k = len(str(N)) 
     sum = 0
