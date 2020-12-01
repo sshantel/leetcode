1cc9f995cf10b3c9e16defe9cce00b297216a539
Chantel Yip <chantelyip422@gmail.com>
2020-11-30 18:08:56 -0800

    initial commit

---
+++ 243_shortest_word_distance.py
@@ -0,0 +1,13 @@
+"""
+Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+
+<<< shortestDistance(["practice", "makes", "perfect", "coding", "makes"], 'coding', 'practice')
+3
+
+<<< shortestDistance(["practice", "makes", "perfect", "coding", "makes"], 'makes', 'practice')
+3 
+
+""" 
+def shortestDistance(words, word1, word2):
+
+ 
