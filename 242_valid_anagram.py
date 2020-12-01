fca5714c1b874935636191a56ce360d5caff9b6c
Chantel Yip <chantelyip422@gmail.com>
2020-11-30 17:59:53 -0800

    added doctests, Runtime: 44 ms, faster than 72.51%

---
+++ 242_valid_anagram.py
@@ -0,0 +1,29 @@
+"""
+Given two strings s and t , write a function to determine if t is an anagram of s.
+
+isAnagram('anagram', 'nagaram')
+True
+
+isAnagram('rat','car')
+False
+
+"""
+
+
+def isAnagram(s, t):
+    l1 = list(s)
+    l2 = list(t)
+    l1.sort()
+    l2.sort()
+    if l1 == l2:
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    import doctest
+
+    if doctest.testmod().failed == 0:
+        print("\n✨ ALL TESTS PASSED!\n")
+
