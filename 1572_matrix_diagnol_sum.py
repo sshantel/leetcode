54e0fa07e4c3452e19ebb501aedfe4dea39818dd
Chantel Yip <chantelyip422@gmail.com>
2021-02-12 23:54:45 -0800

    added doctests

--- 1572_matrix_diagnol_sum.py
+++ 1572_matrix_diagnol_sum.py
@@ -4,6 +4,17 @@ Given a square matrix mat, return the sum of the matrix diagonals.
 
 Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
 
+>>> diagnolSum([[1,2,3],
+              [4,5,6],
+              [7,8,9]])
+25
+
+>>> diagnolSum([[1,1,1,1],
+              [1,1,1,1],
+              [1,1,1,1],
+              [1,1,1,1]])
+8
+
 """
 
 def diagonalSum(max):
