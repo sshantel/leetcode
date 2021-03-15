1dec0bdc5a57b551929e08e5e4dc17733dce1444
Chantel Yip <chantelyip422@gmail.com>
2021-03-15 00:33:15 -0700

    intializing, key error D

--- 76_minimum_window_substring.py
+++ 76_minimum_window_substring.py
@@ -10,3 +10,25 @@ Example 1:
 Input: s = "ADOBECODEBANC", t = "ABC"
 Output: "BANC"
 """
+
+ def minWindow(s, t):
+    #sliding window approach 
+    seen = {}
+    for letter in t:
+        if letter not in seen:
+            seen[letter] = 1
+        else:
+            seen[letter] += 1
+    
+    left = 0
+    for right in range(len(s)):
+        if s[right] in seen: 
+            seen[s[right]] -= 1 
+        if seen[s[right]] < 1:
+            del seen[s[right]] 
+            
+            
+            
+    print(seen)
+print(minWindow("ADOBECODEBANC", "ABC"))
+        
