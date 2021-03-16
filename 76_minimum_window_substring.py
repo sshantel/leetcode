681ac1690d08c350b601b750f271c79b0b7889b7
Chantel Yip <chantelyip422@gmail.com>
2021-03-15 22:43:00 -0700

    working on it!

--- 76_minimum_window_substring.py
+++ 76_minimum_window_substring.py
@@ -3,16 +3,13 @@ Given two strings s and t, return the minimum window in s which will contain all
 
 Note that If there is such a window, it is guaranteed that there will always be only one unique minimum window in s.
 
- 
-
 Example 1:
 
 Input: s = "ADOBECODEBANC", t = "ABC"
 Output: "BANC"
 """
 
-def minWindow(s, t):
-    #sliding window approach 
+def minWindow(s, t):  
     def create_dict(s, t):
         seen = {}
         for letter in t:
@@ -23,19 +20,24 @@ def minWindow(s, t):
         return seen
     
     def sliding_window(s, t):
-        seen = create_dict(s,t) 
         left = 0
         length = []
-        length_count = 0
-        for right in range(len(s)): 
+        length_count = []
+        seen = create_dict(s,t) 
+        for right in range(len(s)):  
+            print(left, right)
             if s[right] in seen:   
-                seen[s[right]] -= 1 
+                seen[s[right]] -= 1  
                 if seen[s[right]] < 1:
                     del seen[s[right]] 
-            if len(seen) == 0:
-                length.append([left, right])
-                length_count = right - left + 1 
-                left += 1
+                    if not seen: 
+                        print(f'left{left} right{right}')
+                        length.append([left, right])  
+                        length_count = right - left + 1  
+                        left = right
+                        seen = create_dict(s, t)
+        
+                 
         return length_count
     return sliding_window(s,t)
 print(minWindow("ADOBECODEBANC", "ABC"))
