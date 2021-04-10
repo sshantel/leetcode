8142313da537dd82e41705967d0df6f9c46450f3
Chantel Yip <chantelyip422@gmail.com>
2021-04-09 21:08:43 -0700

    edited file name

---
+++ 20_valid_parentheses.py
@@ -0,0 +1,42 @@
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+
+""" 
+import unittest
+
+class SimpleTest(unittest.TestCase):
+    def test(self):
+        self.assertTrue(True)
+        
+def isValid(s):
+        #create hashmap of the matching brackets as key, value pairs
+        #create list of open brackets only
+        #loop through each bracket in s
+        #create a stack
+        #if the character is an open character: append it to the stack 
+        #elif the stack isnt empty and the letter is matching the pair to the hashmap
+        #else return false
+        #return stack == []
+        
+        map_brackets = {'(':')', '{':'}', '[':']'}
+        open_brackets = ['(', '[', '{']
+        stack = []
+        for i in s:
+            if i in open_brackets:
+                stack.append(i)
+            elif stack and i == map_brackets[stack[-1]]:
+                stack.pop()
+            else:
+                return False
+        return stack == []
+print(isValid("()"))
+print(isValid("()"))
+
+if __name__ == '__main__':
+    unittest.main()
+    
