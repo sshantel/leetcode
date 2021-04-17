50db5c9c43ca452d8ea042ca989e8c293cf2bfc1
Chantel Yip <chantelyip422@gmail.com>
2021-04-17 11:47:31 -0700

    TypeError float int issue

--- 204_count_primes.py
+++ 204_count_primes.py
@@ -2,3 +2,11 @@
 Count the number of prime numbers less than a non-negative number, n.
 
 """
+def countPrimes(n):
+    primes = [] 
+    print(type(n))
+    print(int(n))
+    for i in range(2,((n)/2)):
+        if i % 2 != 0:
+            primes.append(i)
+    return len(primes)
